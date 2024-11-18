4c6fcc68e7d9b4714a647f715d46794310ef837c
LeticiaMaced <leticia.lcs347@gmail.com>
2024-11-18 15:58:53 -0300

    ex-4

---
+++ JAVASCRIPT/EX4JAVASCRIPT/ex1.js
@@ -0,0 +1,11 @@
+// Crie um array com o nome de 5 jogos. Esse array não pode ter nomes
+// repetidos. Utilizando o método foreach, percorra o array e mostre os
+// valores na tela
+
+
+let array = ["jogo1", "jogo2", "jogo3", "jogo4", "jogo5"]
+
+array.forEach(array => {
+    console.log(array);
+});
+
